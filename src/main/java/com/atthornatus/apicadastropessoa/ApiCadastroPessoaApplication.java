bd05e9d86b9cf5cafb62796e2f2ae7cc51bcebc1
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-02-11 23:00:53 -0300

    Criação do Projeto

---
+++ src/main/java/com/atthornatus/apicadastropessoa/ApiCadastroPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.atthornatus.apicadastropessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroPessoaApplication.class, args);
+	}
+
+}
