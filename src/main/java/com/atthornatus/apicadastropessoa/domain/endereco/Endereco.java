abec9968fa30218e0816b0fb8a72352e4501c82e
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-02-11 23:44:29 -0300

    Criando entidades e configurando BD h2

---
+++ src/main/java/com/atthornatus/apicadastropessoa/domain/endereco/Endereco.java
@@ -0,0 +1,31 @@
+package com.atthornatus.apicadastropessoa.domain.endereco;
+
+import com.atthornatus.apicadastropessoa.domain.pessoa.Pessoa;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Table(name="enderecos")
+@Entity(name="Endereco")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logradouro;
+    private String cep;
+    private String numero;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="cliente_id")
+    private Pessoa pessoa;
+
+    private String cidade;
+}
---
+++ src/main/java/com/atthornatus/apicadastropessoa/domain/pessoa/Pessoa.java
@@ -0,0 +1,36 @@
+package com.atthornatus.apicadastropessoa.domain.pessoa;
+
+import com.atthornatus.apicadastropessoa.domain.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "pessoas")
+@Entity(name = "Pessoa")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDeNascimento;
+    @OneToMany(mappedBy="pessoa", cascade=CascadeType.ALL)
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    public Pessoa(Long id,String nome, LocalDate dataDeNascimento) {
+       this.id = id;
+       this.nome = nome;
+       this.dataDeNascimento = dataDeNascimento;
+    }
+}
